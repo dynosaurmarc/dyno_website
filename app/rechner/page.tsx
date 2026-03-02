a00e5e6413b881f7845f2354732c5f4f86b8e5d9
dynosaurmarc <marc@heydyno.de>
2026-03-02 14:49:33 +0100

    Update page.tsx

--- app/rechner/page.tsx
+++ app/rechner/page.tsx
@@ -1,9 +1,5 @@
 import DynoRechner from "../../components/DynoRechner"
 
 export default function RechnerPage() {
-  return (
-    <main style={{ maxWidth: 980, margin: "0 auto", padding: 24 }}>
-      <DynoRechner showChart />
-    </main>
-  )
+  return <DynoRechner showChart />
 }
